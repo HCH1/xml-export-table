82cb2272b4cb9a4218f6918464de607c395e48c3
HCH1 <theone_730801@hotmail.com>
2016-10-11 23:20:00 +0800

    Update demo_xmlSplitV3.py

--- demo_xmlSplitV3.py
+++ demo_xmlSplitV3.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
-file = open('/Users/HCHung/Documents/2016demo/0912testXML/ch34', "r")    
+file = open('.xml', "r")    
 line = file.read()    
 exp = re.compile(r'<.*?>')
 text_only = exp.sub('',line).strip() #for all <*>, do replace as ''
